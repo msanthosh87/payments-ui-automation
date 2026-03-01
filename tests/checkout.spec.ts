22e9416b954bea1f7574fa4fedc3b1040448c233
msanthosh87 <msanthosh87@gmail.com>
2026-03-01 12:09:56 +0530

    corrected test case id as per review comments

--- tests/checkout.spec.ts
+++ tests/checkout.spec.ts
@@ -2,7 +2,7 @@
 
 import { test, expect } from '@playwright/test';
 
-test('TC-03 has title', async ({ page }) => {
+test('TC-02 has title', async ({ page }) => {
   await page.goto('https://playwright.dev/');
 
   // Expect a title "to contain" a substring.
